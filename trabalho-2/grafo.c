1db1f535beda3b39b60bf8224775b19d72d5817f
root <root@localhost.localdomain>
2016-11-13 04:26:07 -0200

    Test 10 passing

--- trabalho-2/grafo.c
+++ trabalho-2/grafo.c
@@ -4,97 +4,139 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct grafo{
+struct grafo
+{
     char *nome;
     unsigned int nv;
-	unsigned int na;
+    unsigned int na;
 };
 
-struct lista{
-	int nNos;
-	no primeiroNo;
+struct vertice{
+    char *nome;
+    int id;
 };
 
-struct no {
-	no proximo;
-	void *conteudo;
+struct lista
+{
+    int nNos;
+    no primeiroNo;
+};
+
+struct no
+{
+    no proximo;
+    void *conteudo;
 };
 
 // Ref a grafo
 
-void le_nome(Agraph_t *g, grafo graph){
+void le_nome(Agraph_t *g, grafo graph)
+{
     int ncpy= strlen(agnameof(g)) < 100 ? strlen(agnameof(g)) : 100;
     char *nome = malloc(100*sizeof(char));
-    if(nome==NULL){ return NULL; }
-	strncpy(nome,agnameof(g),ncpy);
-	graph->nome=nome;
+    if(nome==NULL)
+    {
+        return NULL;
+    }
+    strncpy(nome,agnameof(g),ncpy);
+    graph->nome=nome;
 }
 
-void le_vertices(Agraph_t *g, grafo graph){
+void le_vertices(Agraph_t *g, grafo graph)
+{
     graph->nv=0;
-	//inicia vertices
-	for (Agnode_t *v=agfstnode(g); v; v=agnxtnode(g,v)){
-		++(graph->nv);
-	}
+    for (Agnode_t *v=agfstnode(g); v; v=agnxtnode(g,v))
+    {
+        ++(graph->nv);
+    }
 }
 
-void le_arestas(Agraph_t *g, grafo graph){
-    graph->nv=0;
-	//inicia vertices
-	for (Agnode_t *v=agfstnode(g); v; v=agnxtnode(g,v)){
-		++(graph->nv);
-	}
+void le_arestas(Agraph_t *g, grafo graph)
+{
+    graph->na=0;
+    for (Agnode_t *v=agfstnode(g); v; v=agnxtnode(g,v))
+    {
+        for (Agedge_t *a=agfstedge(g,v); a; a=agnxtedge(g,a,v))
+        {
+            if (v == agtail(a))
+            {
+                graph->na++;
+            }
+        }
+    }
+
 }
 
-grafo le_grafo(FILE *input){
-	grafo graph = (grafo)malloc(sizeof(grafo));
-	if(!graph){
-		printf("Impossivel alocar memoria para grafo");
-		return NULL;
-	}
-	if (input == 0){
-		printf("le_grafo: Entrada invalida\n");
-		return 0;
-	}
-	Agraph_t *g = agread(input, NULL);
-	if ( !g ){
-		printf("Impossivel ler grafo de arquivo\n");
-		return 0;
-	}
+grafo le_grafo(FILE *input)
+{
+    grafo graph = (grafo)malloc(sizeof(grafo));
+    if(!graph)
+    {
+        printf("Impossivel alocar memoria para grafo");
+        return NULL;
+    }
+    if (input == 0)
+    {
+        printf("le_grafo: Entrada invalida\n");
+        return 0;
+    }
+    Agraph_t *g = agread(input, NULL);
+    if ( !g )
+    {
+        printf("Impossivel ler grafo de arquivo\n");
+        return 0;
+    }
     le_nome(g,graph);
     le_vertices(g,graph);
-	agclose(g);
-	return graph;
+    le_arestas(g,graph);
+    agclose(g);
+    return graph;
 }
 
-char *nome_grafo(grafo g){
-	return g->nome;
+char *nome_grafo(grafo g)
+{
+    return g->nome;
 }
 
-unsigned int numero_vertices(grafo g){
-	return g->nv;
+unsigned int numero_vertices(grafo g)
+{
+    return g->nv;
 }
 
-unsigned int numero_arestas(grafo g){
-	return g->na;
+unsigned int numero_arestas(grafo g)
+{
+    return g->na;
+}
+
+// Ref a vértice
+
+char * nome_vertice(vertice v){
+
+}
+
+char * vertice_nome(char* s, grafo g){
 }
 
 // Ref a lista
 
-lista constroi_lista(void){
-	lista l = (lista) malloc( sizeof(lista) );
-	if (l == NULL){
-		printf("Unable to allocate memory\n");
-	}
-	l->primeiroNo=NULL;
-	return l;
+lista constroi_lista(void)
+{
+    lista l = (lista) malloc( sizeof(lista) );
+    if (l == NULL)
+    {
+        printf("Unable to allocate memory\n");
+    }
+    l->primeiroNo=NULL;
+    return l;
 }
 
-int destroi_lista(lista l, int destroi(void *)){
-	free(l);
-	return 1;
+int destroi_lista(lista l, int destroi(void *))
+{
+    free(l);
+    return 1;
 }
 
-no primeiro_no(lista l){
-	return l->primeiroNo;
+no primeiro_no(lista l)
+{
+    return l->primeiroNo;
 }
--- trabalho-2/test.c
+++ trabalho-2/test.c
@@ -6,135 +6,174 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char msg[100];
 int tests_run = 0;
 int pListSize = 3;
-param pList [] = { { .filename="sample", .nVertex=12, .nEdge=15 }, { .filename="samples/petersen.dot", .nVertex=10, .nEdge=10 }, { .filename="samples/heawood.dot", .nVertex=14, .nEdge=14 } };
-char msg[100];
+param pList [] =
+{
+    { .filename="sample", .nVertex=12, .nEdge=12 },
+    { .filename="samples/petersen.dot", .nVertex=10, .nEdge=15 },
+    { .filename="samples/heawood.dot", .nVertex=14, .nEdge=21 }
+};
 
-static char* testGraphLoad(){
-	FILE *fp = fopen(pList[0].filename,"r");
-	grafo grf = le_grafo(fp);
-	mu_assert("Test 1 - Error: Graph is null",grf!=NULL);
-	fclose(fp);
-	return 0;
-}
-
-static char* testGraphName(){
-	FILE *fp = fopen(pList[0].filename,"r");
-	grafo grf = le_grafo(fp);
-	rewind(fp);
-    	Agraph_t *g = agread(fp, NULL);
-	char * found = nome_grafo(grf);
-	char * expected = agnameof(g);
-	char * message = (char * ) malloc(100 * sizeof(char));
-	sprintf( message, "Test 2 - Found: %s Expected: %s" , found, expected);
-	mu_assert(message,strcmp(found,expected)==0);
-	free(message);
-	agclose(g);
-	fclose(fp);
-	return 0;
-}
-
-static char* testCreateEmptyList(){
-	lista l= constroi_lista();
-	mu_assert("Test 3 - Unable to start list", l!=NULL);
-	return 0;
-}
-
-static char* testDestroyEmptyList(){
-	lista l= constroi_lista();
-	int ok = destroi_lista(l,NULL);
-	mu_assert("Test 4 - Unable to start list", ok);
-	return 0;
-}
-
-static char* primeiroNoNulo(){
-	lista l= constroi_lista();
-	no n = primeiro_no(l);
-	mu_assert("Test 5 - First node should be null ", n==NULL);
-	destroi_lista(l,NULL);
-	return 0;
-}
-
-static char* primeiroNoNaoNulo(){
-	lista l= constroi_lista();
-	no n = primeiro_no(l);
-	no n1 = malloc(sizeof(no));
-	if (n1 == NULL) { return "unable to allocate node"; }
-	mu_assert("Test 6 - First node should not be null ", n1!=NULL);
-	destroi_lista(l,NULL);
-	return 0;
-}
-
-static char* testNumeroVertices(){
-	FILE *fp = fopen(pList[0].filename,"r");
-	grafo grf = le_grafo(fp);
-	mu_assert("Test 7 - Error: Graph vertex number is not the same as input",numero_vertices(grf)==pList[0].nVertex);
-	fclose(fp);
-	return 0;
-}
-
-static char* testNumeroVertices10vezes(){
+static char* testGraphLoad()
+{
     FILE *fp = fopen(pList[0].filename,"r");
-	for(int i = 1; i < 11; i++){
-		grafo grf = le_grafo(fp);
-		sprintf(msg,"Test 8 - Error: Graph vertex number is not the same as input after %d attempts", i);
-		mu_assert(msg,numero_vertices(grf)==pList[0].nVertex);
-		rewind(fp);
-	}
-	fclose(fp);
-	return 0;
+    grafo grf = le_grafo(fp);
+    mu_assert("Test 1 - Error: Graph is null",grf!=NULL);
+    fclose(fp);
+    return 0;
 }
 
-static char* testNumeroVerticesAllGraphs(){
-	for( int i = 0; i < pListSize; i++){
-		FILE *fp = fopen(pList[i].filename,"r");
-		grafo grf = le_grafo(fp);
-		char * message = (char * ) malloc(100 * sizeof(char));
-		sprintf(message, "Test 9 - Filename: %s Expected: %d, Found: %d ", pList[i].filename, pList[i].nVertex,  numero_vertices(grf));
-		mu_assert(message,numero_vertices(grf)==pList[i].nVertex);
-		free(grf);
-		fclose(fp);
-	}
-	return 0;
+static char* testGraphName()
+{
+    FILE *fp = fopen(pList[0].filename,"r");
+    grafo grf = le_grafo(fp);
+    rewind(fp);
+    Agraph_t *g = agread(fp, NULL);
+    char * found = nome_grafo(grf);
+    char * expected = agnameof(g);
+    char * message = (char * ) malloc(100 * sizeof(char));
+    sprintf( message, "Test 2 - Found: %s Expected: %s", found, expected);
+    mu_assert(message,strcmp(found,expected)==0);
+    free(message);
+    agclose(g);
+    fclose(fp);
+    return 0;
 }
 
-static char* testNumeroArestasAllGraphs(){
-	for( int i = 0; i < pListSize; i++){
-		FILE *fp = fopen(pList[i].filename,"r");
-		grafo grf = le_grafo(fp);
-		char * message = (char * ) malloc(100 * sizeof(char));
-		sprintf(message, "Test 10 - Filename: %s Expected: %d, Found: %d ", pList[i].filename, pList[i].nEdge,  numero_arestas(grf));
-		mu_assert(message,numero_arestas(grf)==pList[i].nVertex);
-		free(grf);
-		fclose(fp);
-	}
-	return 0;
+static char* testCreateEmptyList()
+{
+    lista l= constroi_lista();
+    mu_assert("Test 3 - Unable to start list", l!=NULL);
+    return 0;
 }
 
-static char * all_tests() {
-	mu_run_test(testGraphLoad);
-	mu_run_test(testGraphName);
-	mu_run_test(testCreateEmptyList);
-	mu_run_test(testDestroyEmptyList);
-	mu_run_test(primeiroNoNulo);
-	mu_run_test(primeiroNoNaoNulo);
-	mu_run_test(testNumeroVertices);
-	mu_run_test(testNumeroVertices10vezes);
-	mu_run_test(testNumeroVerticesAllGraphs);
-    mu_run_test(testNumeroArestasAllGraphs);
-	return 0;
+static char* testDestroyEmptyList()
+{
+    lista l= constroi_lista();
+    int ok = destroi_lista(l,NULL);
+    mu_assert("Test 4 - Unable to start list", ok);
+    return 0;
 }
 
-int main(int argc, char **argv) {
-	char *result = all_tests();
-	if (result != 0) {
-		printf("%s\n", result);
-	}
-	else {
-		printf("ALL TESTS PASSED\n");
-	}
-	printf("Tests run: %d\n", tests_run);
+static char* primeiroNoNulo()
+{
+    lista l= constroi_lista();
+    no n = primeiro_no(l);
+    mu_assert("Test 5 - First node should be null ", n==NULL);
+    destroi_lista(l,NULL);
+    return 0;
+}
 
-	return result != 0;
+static char* primeiroNoNaoNulo()
+{
+    lista l= constroi_lista();
+    no n = primeiro_no(l);
+    no n1 = malloc(sizeof(no));
+    if (n1 == NULL)
+    {
+        return "unable to allocate node";
+    }
+    mu_assert("Test 6 - First node should not be null ", n1!=NULL);
+    destroi_lista(l,NULL);
+    return 0;
+}
+
+static char* testNumeroVertices()
+{
+    FILE *fp = fopen(pList[0].filename,"r");
+    grafo grf = le_grafo(fp);
+    mu_assert("Test 7 - Error: Graph vertex number is not the same as input",numero_vertices(grf)==pList[0].nVertex);
+    fclose(fp);
+    return 0;
+}
+
+static char* testNumeroVertices10vezes()
+{
+    FILE *fp = fopen(pList[0].filename,"r");
+    for(int i = 1; i < 11; i++)
+    {
+        grafo grf = le_grafo(fp);
+        sprintf(msg,"Test 8 - Error: Graph vertex number is not the same as input after %d attempts", i);
+        mu_assert(msg,numero_vertices(grf)==pList[0].nVertex);
+        rewind(fp);
+    }
+    fclose(fp);
+    return 0;
+}
+
+static char* testNumeroVerticesAllGraphs()
+{
+    for( int i = 0; i < pListSize; i++)
+    {
+        FILE *fp = fopen(pList[i].filename,"r");
+        grafo grf = le_grafo(fp);
+        char * message = (char * ) malloc(100 * sizeof(char));
+        sprintf(message, "Test 9 - Filename: %s Expected: %d, Found: %d ", pList[i].filename, pList[i].nVertex,  numero_vertices(grf));
+        mu_assert(message,numero_vertices(grf)==pList[i].nVertex);
+        free(grf);
+        fclose(fp);
+    }
+    return 0;
+}
+
+static char* testNumeroArestasAllGraphs()
+{
+    for( int i = 0; i < pListSize; i++)
+    {
+        FILE *fp = fopen(pList[i].filename,"r");
+        grafo grf = le_grafo(fp);
+        char * message = (char * ) malloc(100 * sizeof(char));
+        sprintf(message, "Test 10 - Filename: %s Expected: %d, Found: %d ", pList[i].filename, pList[i].nEdge, numero_arestas(grf));
+        mu_assert(message,numero_arestas(grf)==pList[i].nEdge);
+        free(grf);
+        fclose(fp);
+    }
+    return 0;
+}
+
+static char* testNomeVertices(){
+    for( int i = 0; i < pListSize; i++)
+    {
+        FILE *fp = fopen(pList[i].filename,"r");
+        grafo grf = le_grafo(fp);
+        char * message = (char * ) malloc(100 * sizeof(char));
+        sprintf(message, "Test 10 - Filename: %s Expected: %d, Found: %d ", pList[i].filename, pList[i].nEdge, numero_arestas(grf));
+        mu_assert(message,numero_arestas(grf)==pList[i].nEdge);
+        free(grf);
+        fclose(fp);
+    }
+    return 0;
+}
+
+static char * all_tests()
+{
+    mu_run_test(testGraphLoad);
+    mu_run_test(testGraphName);
+    mu_run_test(testCreateEmptyList);
+    mu_run_test(testDestroyEmptyList);
+    mu_run_test(primeiroNoNulo);
+    mu_run_test(primeiroNoNaoNulo);
+    mu_run_test(testNumeroVertices);
+    mu_run_test(testNumeroVertices10vezes);
+    mu_run_test(testNumeroVerticesAllGraphs);
+    mu_run_test(testNomeVertices());
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    char *result = all_tests();
+    if (result != 0)
+    {
+        printf("%s\n", result);
+    }
+    else
+    {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
 }
